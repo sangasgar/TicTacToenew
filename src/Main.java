220f2f9c185f478279cbe9ebdb52bba060e83832
Garegin <sangas@yandex.ru>
2020-08-15 20:24:14 +0300

    Полная версия Крестики-Нолики

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Main {
+    public static void main(String[] args) {
+        new GameWindows();
+    }
 }
